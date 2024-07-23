cdc05818e28b088b4cb97c02a3839a8bce3c33a9
FaizalFeng <fzx401@gmail.com>
2024-07-23 23:28:33 +0800

    init

---
+++ src/27.移除元素.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=cpp
+ *
+ * [27] 移除元素
+ */
+
+// @lc code=start
+#include <iostream>
+#include <vector>
+class Solution {
+public:
+    int removeElement(std::vector<int>& nums, int val)
+    {
+        int slow = 0;
+        int fast = 0;
+        while (fast < nums.size()) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+                fast++;
+            } else {
+                fast++;
+            }
+        }
+        return slow;
+    }
+};
+// @lc code=end
+
+int main()
+{
+    Solution s;
+    std::vector<int> nums = { 0, 1, 2, 2, 3, 0, 4, 2 };
+    int res = s.removeElement(nums, 2);
+    std::cout << res << std::endl;
+}
