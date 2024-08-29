4ce3458356e7f80f557da360ee5780202eea4bbf
FaizalFeng <fzx401@gmail.com>
2024-08-29 08:56:00 +0800

    修正空指针

--- 链表/24.两两交换链表节点.cpp
+++ 链表/24.两两交换链表节点.cpp
@@ -20,7 +20,7 @@ class Solution
   public:
     ListNode *swapPairs(ListNode *head)
     {
-        if (head->next == NULL || head == NULL)
+        if (head == NULL || head->next == NULL)
         {
             return head;
         }
@@ -36,4 +36,27 @@ class Solution
 
 int main()
 {
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    Solution s;
+    ListNode *res = s.swapPairs(head);
+
+    while (res != nullptr)
+    {
+        std::cout << res->val << " ";
+        res = res->next;
+    }
+    std::cout << std::endl;
+
+    // 释放内存
+    while (head != nullptr)
+    {
+        ListNode *temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    return 0;
 }
